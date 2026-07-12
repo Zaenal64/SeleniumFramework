1e962d25b4bf20e3922958c9720ba6bfd7347137
zaenal fahmi <zaenalfahmi34@gmail.com>
2026-07-12 13:55:05 +0700

    added Data Driven With Excel

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -5,6 +5,8 @@ import utils.Log;
 import org.openqa.selenium.WebDriver;
 import java.time.Duration;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -12,34 +14,51 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class LoginPage {
 	private WebDriver driver;
 
-	private By passwordTextBox = By.name("password");
-	private By loginButton = By.cssSelector("button[type='submit']");
-	private By textInvalidCredentials = By.xpath("//p[@class='oxd-text oxd-text--p oxd-alert-content-text']");
+	@FindBy(name="username")
+	WebElement usernameTextbox;
+	
+	@FindBy(name="password")
+	WebElement passwordTesxtBox;
+	
+	@FindBy(css = "button[type='submit']")
+	WebElement loginButton;
+	
+	@FindBy(xpath = "//p[@class='oxd-text oxd-text--p oxd-alert-content-text']")
+	WebElement textInvalidCredentials;
+	
+	
+//	private By passwordTextBox = By.name("password");
+//	private By loginButton = By.cssSelector("button[type='submit']");
+//	private By textInvalidCredentials = By.xpath("//p[@class='oxd-text oxd-text--p oxd-alert-content-text']");
 
 
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
 
 	public void enterUsername(String username) {
 		Log.info("Entering Username");
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		WebElement emailField = wait.until(ExpectedConditions.elementToBeClickable(By.name("username")));
+		WebElement emailField = wait.until(ExpectedConditions.elementToBeClickable(usernameTextbox));
 		emailField.clear();
 		emailField.sendKeys(username);
 	}
 
 	public void enterPassword(String password) {
 		Log.info("Entering Password");
-		driver.findElement(passwordTextBox).clear();
-		driver.findElement(passwordTextBox).sendKeys(password);
+		passwordTesxtBox.clear();
+		passwordTesxtBox.sendKeys(password);
+//		driver.findElement(passwordTextBox).clear();
+//		driver.findElement(passwordTextBox).sendKeys(password);
 	}
 
 	public void clickLogin() {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		wait.until(ExpectedConditions.elementToBeClickable(loginButton));
 		Log.info("Clicking login button.");
-		driver.findElement(loginButton).click();
+		loginButton.click();
+//		driver.findElement(loginButton).click();
 	}
 	
 	public String getInvalidCredentialsMessage() {
@@ -47,7 +66,7 @@ public class LoginPage {
 
 		Log.info("Invalid Credentials");
 		return wait.until(ExpectedConditions
-	            .visibilityOfElementLocated(textInvalidCredentials))
+	            .visibilityOf(textInvalidCredentials))
 	            .getText();
 	}
 
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,64 +1,99 @@
 package tests;
 
+import java.io.IOException;
+
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
 import utils.Log;
+import utils.ExcelUtils;
 import utils.ExtentReportManager;
 
 public class LoginTest extends BaseTest {
+
+	@DataProvider(name = "LoginData")
+	public Object[][] getLoginData() throws IOException {
+		String filePath = System.getProperty("user.dir") + "/testdata/testAutomation.xlsx";
+		ExcelUtils.loadExcel(filePath, "testAutomation");
+		int rowCount = ExcelUtils.getRowCount();
+		Object[][] data = new Object[rowCount - 1][2];
+
+		for (int i = 1; i < rowCount; i++) {
+			data[i - 1][0] = ExcelUtils.getCellData(i, 0); // Username
+			data[i - 1][1] = ExcelUtils.getCellData(i, 1); // Password
+		}
+		ExcelUtils.closeExcel();
+		return data;
+	}
 	
+//	@DataProvider(name="LoginData2")
+//	public Object[][] getData(){
+//		
+//		return new Object[][] {
+//			{"user1", "pass1"},
+//			{"user2", "pass2"},
+//			{"Admin", "admin123"}
+//		};
+//	}
+
+	
+//	@Test(dataProvider = "LoginData2")
 	@Test
-	public void testValidLogin() {
+	@Parameters({"username","password"})
+	public void testValidLogin(String username, String password) {
 
 		Log.info("Starting login test...");
-		test = ExtentReportManager.createTest("Login Test");
-		
+		test = ExtentReportManager.createTest("Login Test " + username);
+
 		test.info("Navigating to URL");
 		LoginPage loginPage = new LoginPage(driver);
-		
-		
+
 		Log.info("Adding Credentials");
 		test.info("Adding Credentials");
-		loginPage.enterUsername("Admin");
-		loginPage.enterPassword("admin123");
+//		loginPage.enterUsername("Admin");
+//		loginPage.enterPassword("admin123");
+		loginPage.enterUsername(username);
+		loginPage.enterPassword(password);
 		test.info("Clicking on Login Button");
 		loginPage.clickLogin();
-		
+
 		System.out.println("Title of the page is : " + driver.getTitle());
+		System.out.println("URl of the page is : " + driver.getCurrentUrl());
 		Log.info("Verifying page title");
 		test.info("Verifying page title");
 		Assert.assertEquals(driver.getTitle(), "OrangeHRM");
-		
+		Assert.assertTrue(driver.getCurrentUrl().contains("/dashboard"));
+
 		test.pass("Login Succesfull");
 	}
-	
+
 	@Test
 	public void testInValidLogin() {
 
 		Log.info("Starting login test...");
 		test = ExtentReportManager.createTest("Login Test with invalid Credentials");
-		
+
 		test.info("Navigating to URL");
 		LoginPage loginPage = new LoginPage(driver);
-		
-		
+
 		Log.info("Adding Credentials");
 		test.info("Adding Credentials");
 		loginPage.enterUsername("Admin123");
 		loginPage.enterPassword("admin123@");
 		test.info("Clicking on Login Button");
 		loginPage.clickLogin();
-		
+
 		System.out.println("Text of the invalid credentials is : " + loginPage.getInvalidCredentialsMessage());
 		Log.info("Verifying Error text invalid credentials");
 		test.info("Verifying Error Message");
-		
+
 		Assert.assertEquals(loginPage.getInvalidCredentialsMessage(), "Invalid credentials..123");
-		
-		test.pass("Login Succesfull");
+
+		test.fail("Login Unsuccesfull");
 	}
-	
+
 }
